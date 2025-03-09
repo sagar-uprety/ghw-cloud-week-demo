6cbe41d31a871ec82ddf866bf3c8fb48617f3a4b
sagar-uprety <sagarupreti100@gmail.com>
2025-03-09 23:54:07 +0000

    init demo

---
+++ app/client/config.js
@@ -0,0 +1,6 @@
+// src/config/config.js
+const apiUrl = import.meta.env.VITE_API_URL;
+
+export default {
+  apiUrl
+};
---
+++ app/server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
+
+// This is a simple route to check if the server is running.
+app.get("/", (req, res) => {
+  res.send("Server is running :)").status(200);
+});
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
