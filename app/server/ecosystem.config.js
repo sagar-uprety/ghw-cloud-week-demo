9ea081b3b7e12f8d2461be693c8e944268a3ceb7
sagar-uprety <sagarupreti100@gmail.com>
2025-03-11 12:17:59 +0000

    add ecosystem config

---
+++ app/server/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [{
+      name: "app",
+      script: "./server.js",
+      env_vm: {
+        NODE_ENV: "development",
+        ATLAS_URI: "mongodb://localhost:27017",
+        PORT: 5050
+      },
+      env_azure: {
+        NODE_ENV: "production",
+        ATLAS_URI: "mongodb+srv://admin:password@ghw-cloud-b2.xrdxz.mongodb.net/?retryWrites=true&w=majority&appName=ghw-cloud-b2",
+        PORT: 5050
+      }
+    }]
+  }
